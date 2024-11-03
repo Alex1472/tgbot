1e4fa8b6e67568ae668dd51d6cb0042603b4730c
alexander.petrovskiy <tula147@yandex.ru>
2024-11-03 20:31:38 +0300

    extract token .env

--- cmd/tgbot/main.go
+++ cmd/tgbot/main.go
@@ -1,7 +1,51 @@
 ﻿package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
 
 func main() {
-	fmt.Println("hello")
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	token := os.Getenv("TOKEN")
+	fmt.Println(token)
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 0,
+	}
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: "+update.Message.Text)
+		//msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
